3a4d71d2b9f7b6d9467292f6ab7773732e9ab168
yo7 <bloom6445@gmail.com>
2019-10-06 21:17:59 +0900

    spanish

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,16 +2,21 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
 // Hello is hello
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
