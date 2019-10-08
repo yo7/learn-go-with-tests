53b7330684dec652f300108d889ee91f6b36bd17
yo7 <bloom6445@gmail.com>
2019-10-08 11:16:05 +0900

    Test sleep before write

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -28,16 +28,24 @@ type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type DefaultSleeper struct{}
 
 func (s *DefaultSleeper) Sleep() {
 	time.Sleep(time.Second * 1)
+}
+
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+var sleep = "sleep"
+var write = "write"
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
