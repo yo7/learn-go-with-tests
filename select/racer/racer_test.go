735ca7de2425efb7c0e11e64a92d5e1f5f5f1858
yo7 <bloom6445@gmail.com>
2019-10-09 11:44:24 +0900

    refactor

--- select/racer/racer_test.go
+++ select/racer/racer_test.go
@@ -19,7 +19,11 @@ func TestRacer(t *testing.T) {
 		defer fastServer.Close()
 
 		want := fastURL
-		got, _ := Racer(slowURL, fastURL)
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
@@ -27,13 +31,11 @@ func TestRacer(t *testing.T) {
 	})
 
 	t.Run("returns an error if a server dosen't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(11 * time.Second)
+		server := makeDelayedServer(25 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
