d6621d255fe02cc237b30032388acc7d1c8d8cbf
yo7 <bloom6445@gmail.com>
2019-10-13 04:01:29 +0900

    roman to arabic

--- property_based_tests/roman_numerals.go
+++ property_based_tests/roman_numerals.go
@@ -7,7 +7,17 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var RomanNumerals = []RomanNumeral{
+type RomanNumerals []RomanNumeral
+
+var romanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
 	{10, "X"},
 	{9, "IX"},
 	{5, "V"},
@@ -15,10 +25,20 @@ var RomanNumerals = []RomanNumeral{
 	{1, "I"},
 }
 
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for _, numeral := range RomanNumerals {
+	for _, numeral := range romanNumerals {
 		for arabic >= numeral.Value {
 			result.WriteString(numeral.Symbol)
 			arabic -= numeral.Value
@@ -26,4 +46,25 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+func ConvertToArabic(roman string) int {
+	total := 0
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if i+1 < len(roman) && symbol == 'I' {
+			nextSymbol := roman[i+1]
+			potentialNumber := string([]byte{symbol, nextSymbol})
+			value := romanNumerals.ValueOf(potentialNumber)
+			if value != 0 {
+				total += value
+				i++ // move past this character too for the next loop
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
 }
