b24fc16b86534c7b23a58d108ab8e0e03a4a27a6
yo7 <bloom6445@gmail.com>
2019-10-13 02:11:33 +0900

    sync

--- sync/counter.go
+++ sync/counter.go
@@ -1,10 +1,19 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value += 1
 }
 
