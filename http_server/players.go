9748177c479a93d3857313f85325eec936746784
yo7 <bloom6445@gmail.com>
2019-10-14 14:41:58 +0900

    multiple players

--- http_server/players.go
+++ http_server/players.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	err := http.ListenAndServe(":5000", handler)
+	if err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	if player == "Pepper" {
+		fmt.Fprintf(w, "20")
+	}
+
+	if player == "Floyd" {
+		fmt.Fprintf(w, "10")
+	}
+}
--- http_server/players_test.go
+++ http_server/players_test.go
@@ -8,13 +8,27 @@ import (
 
 func TestGetPlayers(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/pepper", nil)
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
 		got := response.Body.String()
 		want := "20"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
