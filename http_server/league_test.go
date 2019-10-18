99e1e67a589a701cf6fe1f3184ff325b17500597
yo7 <bloom6445@gmail.com>
2019-10-18 13:23:29 +0900

    refactor

--- http_server/league_test.go
+++ http_server/league_test.go
@@ -18,22 +18,41 @@ func TestLeague(t *testing.T) {
 		store := StubPlayerStore{league: wantedLeague}
 		server := NewPlayerServer(&store)
 
-		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		request := newLeagueRequest()
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 
-		var got []Player
+		got := getLeagueFromResponse(t, response)
 
-		err := json.NewDecoder(response.Body).Decode(&got)
-
-		if err != nil {
-			t.Errorf("Unable to parse response from server %q into slice of Player, '%v'", response.Body, got)
-		}
 		assertStatus(t, response.Code, http.StatusOK)
-
+		assertLeague(t, got, wantedLeague)
 		if !reflect.DeepEqual(got, wantedLeague) {
 			t.Errorf("got %v want %v", got, wantedLeague)
 		}
-
 	})
+}
+
+func getLeagueFromResponse(t *testing.T, response *httptest.ResponseRecorder) (league []Player) {
+	t.Helper()
+
+	err := json.NewDecoder(response.Body).Decode(&league)
+
+	if err != nil {
+		t.Errorf("Unable to parse response from server %q into slice of Player, '%v'", response.Body, league)
+	}
+
+	return
+}
+
+func assertLeague(t *testing.T, got, wanted []Player) {
+	t.Helper()
+
+	if !reflect.DeepEqual(got, wanted) {
+		t.Errorf("got %v want %v", got, wanted)
+	}
+}
+
+func newLeagueRequest() *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+	return request
 }
